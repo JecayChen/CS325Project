e6f055b470dc0c49888e655b16442ccd1d1cff06
JecayChen <chenjecay@gmail.com>
2025-03-17 12:44:16 -0500

    added comments

--- main.py
+++ main.py
@@ -44,37 +44,49 @@ def llmQuery(modelName):
     except subprocess.CalledProcessError as e:
         print("Error while calling Ollama:", e)
 
+#code for project requirements
 def project():
+    #prep for sentiment query
     sentimentQuery = "Respond with concisely only either negative, positive, or neutral sentiment of this statement:"
+
+    #file reading
     fileName = input("     Input file name:\n\n")
     fileLines = []
     with open(fileName, 'r') as file:
         for line in file:
             fileLines.append(line.strip())
 
+    #clean response text
     with open('response.txt', 'w') as file:
             file.write("")
 
+    #models being used
     modelNames = ["phi3.5", "gemma3"]
 
+    #cycle through models
     for name in modelNames:
         modelName = name
         stopCommand = ["ollama", "stop", modelName]
 
+        #cycle through text prompts
         for fileLine in fileLines:
             line = str(fileLine)
             llmPrompt = sentimentQuery + " " + line
             queryCommand = ["ollama", "run", modelName, llmPrompt]
 
+            #llm query
             try:
                 result = subprocess.run(queryCommand, capture_output=True, text=True, check=True)
                 fileInput = "     " + modelName + " Response to " + line + ":\n\n" + result.stdout
+
+                #write to file
                 with open('response.txt', 'a') as file:
                     file.write(fileInput)
 
             except subprocess.CalledProcessError as e:
                 print("Error while calling Ollama:", e)
 
+        #stop llm running afterwards
         subprocess.run(stopCommand, text=True, check=True)
 
 if __name__ == "__main__":
