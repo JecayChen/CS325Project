45d3f8e3691876e2676179d2bc45f6f16b8fc5ef
JecayChen <chenjecay@gmail.com>
2025-03-18 09:35:45 -0500

    edited program text output

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
 def requestAI():
     #user select
     while True:
-        userSelect = input("     Select(Case Sensitive): phi, gemma, project, or quit:\n\n")
+        userSelect = input("[Select(Case Sensitive): phi, gemma, project, or quit:]\n")
         if(userSelect == "quit"):
             quit()
         elif(userSelect == "phi"):
@@ -29,7 +29,7 @@ def llmQuery(modelName):
     
     #setup for CLI query
     sentimentQuery = "Respond with concisely only either negative, positive, or neutral sentiment of this statement:"
-    userPrompt = input("     What is your sentiment query?\n\n")
+    userPrompt = input("[What is your sentiment query?]\n")
     llmPrompt = sentimentQuery + " " + userPrompt
 
     queryCommand = ["ollama", "run", modelName, llmPrompt]
@@ -38,11 +38,11 @@ def llmQuery(modelName):
     #attempt to query for response
     try:
         result = subprocess.run(queryCommand, capture_output=True, text=True, check=True)
-        print("     ", modelName, " Response:\n\n", result.stdout)
+        print("[", modelName, " Response:]\n\n", result.stdout)
         subprocess.run(stopCommand, text=True, check=True)
 
     except subprocess.CalledProcessError as e:
-        print("Error while calling Ollama:", e)
+        print("[Error while calling Ollama:]", e)
 
 #code for project requirements
 def project():
@@ -50,7 +50,7 @@ def project():
     sentimentQuery = "Respond with concisely only either negative, positive, or neutral sentiment of this statement:"
 
     #file reading
-    fileName = input("     Input file name:\n\n")
+    fileName = input("[Input file name:]\n")
     fileLines = []
     with open(fileName, 'r') as file:
         for line in file:
@@ -77,14 +77,14 @@ def project():
             #llm query
             try:
                 result = subprocess.run(queryCommand, capture_output=True, text=True, check=True)
-                fileInput = "     " + modelName + " Response to " + line + ":\n\n" + result.stdout
+                fileInput = "[" + modelName + " Response to \"" + line + "\":]\n" + result.stdout
 
                 #write to file
                 with open('response.txt', 'a') as file:
                     file.write(fileInput)
 
             except subprocess.CalledProcessError as e:
-                print("Error while calling Ollama:", e)
+                print("[Error while calling Ollama:]", e)
 
         #stop llm running afterwards
         subprocess.run(stopCommand, text=True, check=True)
