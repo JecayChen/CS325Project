46913680e01460243aa924eb0fe61bc953368416
JecayChen <chenjecay@gmail.com>
2025-04-13 07:14:57 -0500

    added files and updated yaml for webscraping

--- main.py
+++ main.py
@@ -1,93 +1,129 @@
 import sys
+import os
 import subprocess #needed for CLI inputs
+from webscraper import Webscraper
 
 def main():
-    modelName = requestAI()
-    llmQuery(modelName)
+    # modelName = requestAI()
+    # llmQuery(modelName)
+    fileName = requestFile()
+    outputTitles(fileName)
 
-def requestAI():
-    #user select
+# def requestAI():
+#     #user select
+#     while True:
+#         userSelect = input("[Select(Case Sensitive): phi, gemma, project, or quit:]\n")
+#         if(userSelect == "quit"):
+#             quit()
+#         elif(userSelect == "phi"):
+#             return "phi3.5"
+#             break
+#         elif(userSelect == "gemma"):
+#             return "gemma3"
+#         elif(userSelect == "project"):
+#             project()
+#             return None
+#         else:
+#             continue
+
+# def llmQuery(modelName):
+#     #for when input is not set
+#     if modelName is None:
+#         sys.exit()
+    
+#     #setup for CLI query
+#     sentimentQuery = "Respond with concisely only either negative, positive, or neutral sentiment of this statement:"
+#     userPrompt = input("[What is your sentiment query?]\n")
+#     llmPrompt = sentimentQuery + " " + userPrompt
+
+#     queryCommand = ["ollama", "run", modelName, llmPrompt]
+#     stopCommand = ["ollama", "stop", modelName]
+
+#     #attempt to query for response
+#     try:
+#         result = subprocess.run(queryCommand, capture_output=True, text=True, check=True)
+#         print("[", modelName, " Response:]\n\n", result.stdout)
+#         subprocess.run(stopCommand, text=True, check=True)
+
+#     except subprocess.CalledProcessError as e:
+#         print("[Error while calling Ollama:]", e)
+
+# #code for project requirements
+# def project():
+#     #prep for sentiment query
+#     sentimentQuery = "Respond with concisely only either negative, positive, or neutral sentiment of this statement:"
+
+#     #file reading
+#     fileName = input("[Input file name:]\n")
+#     fileLines = []
+#     with open(fileName, 'r') as file:
+#         for line in file:
+#             fileLines.append(line.strip())
+
+#     #clean response text file
+#     with open('response.txt', 'w') as file:
+#             file.write("")
+
+#     #models being used
+#     modelNames = ["phi3.5", "gemma3"]
+
+#     #cycle through models
+#     for name in modelNames:
+#         modelName = name
+#         stopCommand = ["ollama", "stop", modelName]
+
+#         #cycle through text prompts
+#         for fileLine in fileLines:
+#             line = str(fileLine)
+#             llmPrompt = sentimentQuery + " " + line
+#             queryCommand = ["ollama", "run", modelName, llmPrompt]
+
+#             #llm query
+#             try:
+#                 result = subprocess.run(queryCommand, capture_output=True, text=True, check=True)
+#                 fileInput = "[" + modelName + " Response to \"" + line + "\":]\n" + result.stdout
+
+#                 #write to file
+#                 with open('response.txt', 'a') as file:
+#                     file.write(fileInput)
+
+#             except subprocess.CalledProcessError as e:
+#                 print("[Error while calling Ollama:]", e)
+
+#         #stop llm running afterwards
+#         subprocess.run(stopCommand, text=True, check=True)
+
+#requests for file name or quit
+def requestFile():
     while True:
-        userSelect = input("[Select(Case Sensitive): phi, gemma, project, or quit:]\n")
-        if(userSelect == "quit"):
+        userInput = input("Please input a file name or 'quit':]\n") #ask user for name or quit
+
+        #file validation or quitting program
+        #if valid, returns the file path
+        #else continue requesting
+        if userInput == 'quit': #check to quit
             quit()
-        elif(userSelect == "phi"):
-            return "phi3.5"
-            break
-        elif(userSelect == "gemma"):
-            return "gemma3"
-        elif(userSelect == "project"):
-            project()
-            return None
-        else:
+        elif os.path.isfile(userInput): #check and returns if true
+            print("File Exists")
+            return userInput
+        else: #loop to beginning of userInput
+            print("File Not Found")
             continue
 
-def llmQuery(modelName):
-    #for when input is not set
-    if modelName is None:
-        sys.exit()
-    
-    #setup for CLI query
-    sentimentQuery = "Respond with concisely only either negative, positive, or neutral sentiment of this statement:"
-    userPrompt = input("[What is your sentiment query?]\n")
-    llmPrompt = sentimentQuery + " " + userPrompt
+def outputTitles(fileName):
+    #clean the response text
+    with open('response.txt', 'w') as writeFile:
+        writeFile.write("")
 
-    queryCommand = ["ollama", "run", modelName, llmPrompt]
-    stopCommand = ["ollama", "stop", modelName]
-
-    #attempt to query for response
-    try:
-        result = subprocess.run(queryCommand, capture_output=True, text=True, check=True)
-        print("[", modelName, " Response:]\n\n", result.stdout)
-        subprocess.run(stopCommand, text=True, check=True)
-
-    except subprocess.CalledProcessError as e:
-        print("[Error while calling Ollama:]", e)
-
-#code for project requirements
-def project():
-    #prep for sentiment query
-    sentimentQuery = "Respond with concisely only either negative, positive, or neutral sentiment of this statement:"
-
-    #file reading
-    fileName = input("[Input file name:]\n")
-    fileLines = []
-    with open(fileName, 'r') as file:
-        for line in file:
-            fileLines.append(line.strip())
-
-    #clean response text file
-    with open('response.txt', 'w') as file:
-            file.write("")
-
-    #models being used
-    modelNames = ["phi3.5", "gemma3"]
-
-    #cycle through models
-    for name in modelNames:
-        modelName = name
-        stopCommand = ["ollama", "stop", modelName]
-
-        #cycle through text prompts
-        for fileLine in fileLines:
-            line = str(fileLine)
-            llmPrompt = sentimentQuery + " " + line
-            queryCommand = ["ollama", "run", modelName, llmPrompt]
-
-            #llm query
-            try:
-                result = subprocess.run(queryCommand, capture_output=True, text=True, check=True)
-                fileInput = "[" + modelName + " Response to \"" + line + "\":]\n" + result.stdout
-
-                #write to file
-                with open('response.txt', 'a') as file:
-                    file.write(fileInput)
-
-            except subprocess.CalledProcessError as e:
-                print("[Error while calling Ollama:]", e)
-
-        #stop llm running afterwards
-        subprocess.run(stopCommand, text=True, check=True)
+    with open(fileName, 'r') as readFile:
+        for line in readFile:
+            scrape = Webscraper(line)
+            titleNames = scrape.scrapeTitles()
+            with open('response.txt', 'a') as writeFile:
+                writeFile.write(f"!Titles for {line}\n")
+                for title in titleNames:
+                    writeFile.write(title + "\n")
+                writeFile.write("\n")
 
 if __name__ == "__main__":
     main()
--- webscraper.py
+++ webscraper.py
@@ -3,11 +3,48 @@ from bs4 import BeautifulSoup
 
 class Webscraper:
     def __init__(self, url):
-        self.url = url
+        self.url = url.strip()
     
 #checks url connection response
-def testURL(self):
-    response = requests.get(self.url)
-    
-    if(response == 200):
-        return True
+#return true if successful
+#return false if unsuccessful
+    def testURL(self):
+        try:
+            response = requests.get(self.url) #attempts connection
+        
+            if response.status_code == 200: #connection successful
+                print(f"Connection Successful: {self.url}")
+                return True
+            else: #connection unsuccessful
+                print(f"Connection Unsuccessful: {self.url}")
+                print(f"Error Code: {response.status_code}")
+                return False
+
+        except requests.exceptions.RequestException as e: #connection error
+            print(f"Error in accessing {self.url}: {e}")
+            return False
+
+    #scrape webpage for article titles
+    #returns a string array of titles
+    def scrapeTitles(self):
+        if self.testURL(): #restURL successful
+            url = self.url
+            try: #try-catch for errors in retrieving titles
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                titles = []
+                MIN_LENGTH = 50
+
+                #scrapes the html for article title links that are at least a certain length long
+                for links in soup.find_all('a', href=True): #searches for all <a href> links
+                    text = links.get_text(strip=True) #get the raw text for each link
+                    if text and len(text) >= MIN_LENGTH: #if the raw text exists and is over minimum characters
+                        titles.append(text) #add if to the title list
+                return titles
+            
+            except Exception as e:
+                print(f"Error in scrapeTitles: {e}")
+                return []
+        else:
+            print(f"Error in connection. Unable to perform scrapeTitles")
+            return []
